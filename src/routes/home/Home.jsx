bc019a8030e185330f2360f6faafcabc067a49ca
TegHocine <arcou98@hotmail.com>
2022-02-17 23:00:04 +0100

    style: todo input style

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,5 +1,20 @@
-import React from 'react'
+import { BsPlus } from 'react-icons/bs'
+import './style/home.css'
 
 export default function home() {
-  return <div>home</div>
+  return (
+    <div className='home'>
+      <div className='home-wrapper'>
+        <div className='home-title'>DO-ME</div>
+        <div className='home-input-wrapper'>
+          <BsPlus className='home-add-icon' />
+          <input
+            type='text'
+            placeholder='Create a new todo ...'
+            className='home-input'
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
